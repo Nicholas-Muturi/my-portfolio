e4197674c0a26945694c14f23ebf698e5cc476e5
Nick Muturi <nick@localhost.localdomain>
2018-07-13 00:09:01 +0300

    Make the cards slide up and down, revealing the content - Finished with the skills section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
 
-  var timeDuration = 2500;
+  $(".card .proj1").click(function(){
+    $(".proj1Body").slideToggle("slow");
+  });
+  $(".card .proj2").click(function(){
+    $(".proj2Body").slideToggle("slow");
+  });
+  $(".card .proj3").click(function(){
+    $(".proj3Body").slideToggle("slow");
+  });
+  $(".card .proj4").click(function(){
+    $(".proj4Body").slideToggle("slow");
+  });
 
+  var timeDuration = 2500;
   $(".chart#html_lvl").easyPieChart({
     size: 180,
     barColor: '#ee6400',
