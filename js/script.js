d791a770662e6509bdf314dfc222a15d992ddf17
Nick Muturi <nick@localhost.localdomain>
2018-07-12 23:14:02 +0300

    Placed bootstrap cards (in place of custom css cards) to view projects

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
 $(document).ready(function(){
 
+  var timeDuration = 2500;
 
   $(".chart#html_lvl").easyPieChart({
     size: 180,
     barColor: '#ee6400',
-    lineWidth: 5,
+    lineWidth: 7,
     scaleColor: false,
+    animate: {
+      duration: timeDuration,
+      enabled: true,
+    },
   });
   $(".chart#js_lvl").easyPieChart({
     size: 180,
     barColor: '#37c605',
-    lineWidth: 5,
+    lineWidth: 7,
     scaleColor: false,
+    animate: {
+      duration: timeDuration,
+      enabled: true,
+    },
   });
   $(".chart#css_lvl").easyPieChart({
     size: 180,
     barColor: '#d71111',
-    lineWidth: 5,
+    lineWidth: 7,
     scaleColor: false,
+    animate: {
+      duration: timeDuration,
+      enabled: true,
+    },
   });
   $(".chart#bootstrap_lvl").easyPieChart({
     size: 180,
     barColor: '#0085ff',
-    lineWidth: 5,
+    lineWidth: 7,
     scaleColor: false,
+    animate: {
+      duration: timeDuration,
+      enabled: true,
+    },
   });
 });
