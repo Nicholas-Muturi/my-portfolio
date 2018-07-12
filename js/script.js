40d6d1fa93ecce7d385af339e20537935428eba0
Nick Muturi <nick@localhost.localdomain>
2018-07-12 20:22:34 +0300

    centered the valued and placed different colours for each pie chart

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
 
 
-  $(".chart").easyPieChart({
+  $(".chart#html_lvl").easyPieChart({
     size: 180,
-    barColor: "#0085ff",
+    barColor: '#ee6400',
+    lineWidth: 5,
+    scaleColor: false,
   });
-
+  $(".chart#js_lvl").easyPieChart({
+    size: 180,
+    barColor: '#37c605',
+    lineWidth: 5,
+    scaleColor: false,
+  });
+  $(".chart#css_lvl").easyPieChart({
+    size: 180,
+    barColor: '#d71111',
+    lineWidth: 5,
+    scaleColor: false,
+  });
+  $(".chart#bootstrap_lvl").easyPieChart({
+    size: 180,
+    barColor: '#0085ff',
+    lineWidth: 5,
+    scaleColor: false,
+  });
 });
